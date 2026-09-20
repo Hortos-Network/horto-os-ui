b67a3e5bf1baaa5785bbee853d59d386a394cc88
gRoussac <gRoussac@users.noreply.github.com>
2026-09-20 00:06:17 +0000

    Deploy documentation for 3707b348a6e2a65800f2315ed3664e23d2ec4053

--- horto_os_ui_shared/remote/ssh/sidebar-items.js
+++ horto_os_ui_shared/remote/ssh/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["default_identity_pubkey","require_ok"],"struct":["SshEnv","SshSession"]};
+window.SIDEBAR_ITEMS = {"fn":["default_identity_pubkey","identity_private_key","require_ok"],"struct":["SshEnv","SshSession"]};
